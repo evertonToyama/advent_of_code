b564b7f2fd584f6f923d01e2043a8b870dfad9a1
evertonToyama <everton.toyama@gmail.com>
2025-12-06 23:57:24 -0300

    2025 - day 3

---
+++ lib/2025/day3/day3.dart
@@ -0,0 +1,27 @@
+import 'package:advent_of_code/day.dart';
+
+class Day3Year2025 extends Day {
+  @override
+  int get day => 3;
+
+  @override
+  int get year => 2025;
+
+  @override
+  Future<Object> part1({String filename = "input.txt", String? input}) {
+    // TODO: implement part1
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Object> part2({String filename = "input.txt", String? input}) {
+    // TODO: implement part2
+    throw UnimplementedError();
+  }
+
+  @override
+  Object loadContent(String content) {
+    // TODO: implement loadContent
+    throw UnimplementedError();
+  }
+}
